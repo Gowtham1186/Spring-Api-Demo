1b4fb36826f872f127cd4417140ca86339834787
Gowtham1186 <105626202+Gowtham1186@users.noreply.github.com>
2024-07-29 12:58:12 +0000

    Updated HelloControler to show the json difference

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,28 +4,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.github.fge.jsonpatch.diff.JsonDiff;
 import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
-    	    @PostMapping("/receiveJsonArrays")
+    	@PostMapping("/receiveJsonArrays")
 	    public String receiveJsonArrays(@RequestBody JsonNode requestBody) {
 	        try {
 	            // Extract two JSON arrays from the request body
 	            JsonNode jsonArray1 = requestBody.get("array1");
 	            JsonNode jsonArray2 = requestBody.get("array2");
 
-	            // Compare JSON objects
-	            if (jsonArray1.equals(jsonArray2)) {
-	                return "JSON objects are equal.";
-	            } else {
-	                return "JSON objects are not equal.";
-	            }
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            return "Error processing JSON: " + e.getMessage();
-	        }
+	            // Calculate the difference between two JSON trees
+				JsonNode patch = JsonDiff.asJson(jsonArray1, jsonArray2);
+				// Compare JSON objects
+				if (jsonArray1.equals(jsonArray2)) {
+					return patch.toPrettyString();
+				} else {
+					return patch.toPrettyString();
+				}
+	        	} catch (Exception e) {
+	           		e.printStackTrace();
+	          	 	return "Error processing JSON: " + e.getMessage();
+	       		}
         }
 }
