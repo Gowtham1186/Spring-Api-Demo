ff26dab630a0e039b1403cc450d24b1bc400dae5
Gowtham1186 <105626202+Gowtham1186@users.noreply.github.com>
2024-07-29 07:29:39 +0000

    Updated HelloController to handle name parameter

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
     }
 }
